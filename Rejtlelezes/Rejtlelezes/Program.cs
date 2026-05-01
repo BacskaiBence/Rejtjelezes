f7dffd506ed68a15eba8b440612f4db3c6457b10
BacskaiBence <bacskaibence2007@gmail.com>
2026-05-01 12:45:45 +0200

    first task completed.

--- Rejtlelezes/Rejtlelezes/Program.cs
+++ Rejtlelezes/Rejtlelezes/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Rejtlelezes
+﻿using System.Security;
+
+namespace Rejtlelezes
 {
     internal class Program
     {
         static Dictionary<char, int> code = new Dictionary<char, int>();
-        
+        static string abc = "abcdefghijklmnopqrstuvwxyz ";
+
+
         static Dictionary<string,string> encryptedMessages = new Dictionary<string, string>();
         static void dataUpload()
         {
@@ -16,74 +20,31 @@
             }
         }
 
-        static string Encrypt(string message)
+        static string Encrypt(string message, string key)
         {
-            Random random = new Random();
-            
-            string key= "";
-            int counter = 0;
             string encryptedMessage ="";
             int currentNumber = 0;
-            string encryptedNumbers = "";
-            string keyNumbers = "";
-            foreach (var item in message)
+
+            for (int i = 0; i < message.Length; i++)
             {
-                string temp = "";
-                if (item==Convert.ToChar(" "))
-                {
-                    key+=" ";
-                    temp = " ";
-                }
-                else
-                {
-                    temp = random.Next(0, 26).ToString();
-                    keyNumbers+= temp+" ";
-                    key += code.FirstOrDefault(x => x.Value == int.Parse(temp.ToString())).Key;
-                    counter++;
-                }
-                currentNumber=code.GetValueOrDefault(item)+int.Parse(temp);
-                if (currentNumber>26)
-                {
-                    currentNumber = (code.GetValueOrDefault(item) + int.Parse(temp)) % 27;
-                }
-                encryptedNumbers += currentNumber+" ";
-                encryptedMessage += code.FirstOrDefault(x => x.Value == int.Parse(currentNumber.ToString())).Key;
-
+                currentNumber = (code.GetValueOrDefault(message[i]) + code.GetValueOrDefault(key[i])) %27;
+                encryptedMessage += abc[currentNumber];
             }
-            encryptedMessages.Add(key, encryptedMessage);
 
-            /*Console.WriteLine(encryptedNumbers);
-            Console.WriteLine(key);
-            Console.WriteLine(keyNumbers);*/
             return encryptedMessage;
         }
 
-        static string Decrypt(string message)
+        static string Decrypt(string message, string key)
         {
-            Random random = new Random();
-
             string decryptedMessage = "";
             int currentNumber = 0;
 
-            foreach (var item in encryptedMessages)
+            for (int i = 0; i < message.Length; i++)
             {
-                if (item.Value==message)
-                {
-                    foreach (var item1 in message)
-                    {
-                        currentNumber= code.GetValueOrDefault(item1)-code.GetValueOrDefault(item.Key[message.IndexOf(item1)]);
-                        if (currentNumber<0)
-                        {
-                            currentNumber = code.GetValueOrDefault(item1) - code.GetValueOrDefault(item.Key[message.IndexOf(item1)]) + 27;
-                        }
-                        decryptedMessage += code.FirstOrDefault(x => x.Value == int.Parse(currentNumber.ToString())).Key;
-                    }
-                }
-               /* Console.WriteLine(item.Value);
-                Console.WriteLine("adsdasd");
-                Console.WriteLine(item.Key);*/
-                
+                currentNumber = (code.GetValueOrDefault(message[i]) - code.GetValueOrDefault(key[i])+27)%27;
+                decryptedMessage += abc[currentNumber];
             }
+                
             return decryptedMessage;
         }
 
@@ -92,20 +53,23 @@
         {
             dataUpload();
 
-
             //string message = "almaz"; -> 0 11 12 0 25
             //string key = "fgdsn"; -> 5 6 3 18 13
             // 5 17 15 18 38%27-> 11
             //string encrypted = "frpsl";
 
-
             while (true)
             {
                 Console.WriteLine("Irj egy uzenetet: ");
                 string message = Console.ReadLine().ToLower();
-                Console.WriteLine(Encrypt(message));
-                string dmessage = Encrypt(message);
-                Console.WriteLine(Decrypt(dmessage));
+                Console.WriteLine("Ird be a kulcsot: ");
+                string key = Console.ReadLine().ToLower();
+                encryptedMessages.Add(key, Encrypt(message, key));
+                Console.WriteLine(Encrypt(message,key));
+                string dmessage = Encrypt(message, key);
+                Console.WriteLine("Ird be a megoldo kulcsot: ");
+                string dkey = Console.ReadLine().ToLower();
+                Console.WriteLine(Decrypt(dmessage,dkey));
             }
 
             
